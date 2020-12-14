f12290cf8babb38ea87c49667f5953e4144cef1a
Liam Baker <liam.baker2310@gmail.com>
2020-12-14 22:00:37 +0900

    added books controller and routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: %i[show search]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    if @book
+      render partial: 'books/book.jbuilder', locals: { book: @book }
+    else
+      not_found
+    end
+  end
+
+  def search
+    return not_found unless @book
+
+    @start = params[:from_date].to_datetime
+    @end = params[:to_date].to_datetime.end_of_day
+    @earnings = @book.borrowed_books
+                     .where(updated_at: @start..@end)
+                     .calculate_earnings
+  end
+
+  private
+
+  def not_found
+    render json: { errors: "Can't find Book" },
+           status: 404
+  end
+
+  def find_book
+    @book = Book.where(id: params[:id]).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :v1 do
     resources :users, only: %i[show create]
+
+    resources :books, only: %i[index show] do
+      member do
+        get 'search'
+      end
+    end
   end
 end
